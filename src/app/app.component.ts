f2b4906860589efdea3cbdf97d5a97e9bef6c84d
Nicholette <nekonekonik@gmail.com>
2016-10-04 15:37:13 +0800

    Display chats as a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './chat/chat.service';
+import { Chat } from './chat/chat';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ChatService],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  newChat: Chat = new Chat();
+  constructor(private chatService: ChatService) {}
+
+  addChat() {
+    console.log('add chat');
+    this.chatService.addChat(this.newChat);
+    this.newChat = new Chat();
+  }
+
+  get chats() {
+    return this.chatService.getAllChats();
+  }
+
+  ngOnInit() {
+  }
 }
